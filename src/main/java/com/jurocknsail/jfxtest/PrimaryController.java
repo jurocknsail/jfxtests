022288bb8d509df26c53239ae96fbfe4dee86bf8
jberger <julien.berger1421@gmail.com>
2022-12-17 22:43:12 +0100

    Table tests

--- src/main/java/com/jurocknsail/jfxtest/PrimaryController.java
+++ src/main/java/com/jurocknsail/jfxtest/PrimaryController.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
